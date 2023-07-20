80f3304ef0aa318c8f3fb57c872f5b51e4ba4cca
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2023-07-20 14:13:01 -0400

    Update multi_Get_vina.py

--- multi_Get_vina.py
+++ multi_Get_vina.py
@@ -5,22 +5,25 @@ import subprocess
 
 protein_path = 'protein_pdbqt'
 
-def process_file(i, rank, protein_path):
-    input_file = f'docked_ligand_pdbqt/{i}/{i}_docked_ligand_rank{rank}.pdbqt'
-    output_file = f'vina_feature/{i}_{rank}.csv'
+def process_file(protein_path, i, rank):
+    input_file = f'/vast/zf2012/diffdock/HSP90_folder_combined/docked_ligand_pdbqt/{i}/{i}_docked_ligand_rank{rank}.pdbqt'
+    output_file = f'/vast/zf2012/diffdock/HSP90_folder_combined/vina_feature/{i}_{rank}.csv'
 
     # Assuming 'cal_vina_features.py' is a standalone script, you can call it like this:
-    command = f"python cal_vina_features.py {protein_path} {input_file} {output_file} {i}"
+    command = f"python cal_vina_features.py {protein_path}/AF-P07900-F1-model_v4.pdbqt {input_file} {output_file} {i}"
+    #print(command)
     subprocess.run(command, shell=True)
 
 def main():
     pool = multiprocessing.Pool()
     func = partial(process_file, protein_path)
 
-    ligand_dirs = os.listdir('docked_ligand_pdbqt')
+    ligand_dirs = os.listdir('/vast/zf2012/diffdock/HSP90_folder_combined/docked_ligand_pdbqt')
 
     for i in ligand_dirs:
+        #print(i)
         for rank in range(1, 6):
+            #print(rank)
             pool.apply_async(func, (i, rank))
 
     pool.close()
