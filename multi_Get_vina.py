b0f7ca66a7ae7b5a95cccf4146c899bcbcba37d9
Zixuan Feng_NYU <76710319+ZixuanFeng-NYU@users.noreply.github.com>
2023-07-20 13:43:32 -0400

    Update multi_Get_vina.py

--- multi_Get_vina.py
+++ multi_Get_vina.py
@@ -1,19 +1,25 @@
 import multiprocessing
+import os
 from functools import partial
-from cal_vina_features import cal_vina_features
+import subprocess
 
 protein_path = 'protein_pdbqt'
 
-def process_file(protein_path, i, rank):
-    input_file = f''
-    output_file = f''
-    return cal_vina_features(protein_path, input_file, output_file, i)
+def process_file(i, rank, protein_path):
+    input_file = f'docked_ligand_pdbqt/{i}/{i}_docked_ligand_rank{rank}.pdbqt'
+    output_file = f'vina_feature/{i}_{rank}.csv'
+
+    # Assuming 'cal_vina_features.py' is a standalone script, you can call it like this:
+    command = f"python cal_vina_features.py {protein_path} {input_file} {output_file} {i}"
+    subprocess.run(command, shell=True)
 
 def main():
     pool = multiprocessing.Pool()
     func = partial(process_file, protein_path)
 
-    for i in os.listdir('docked_ligand_pdbqt'):
+    ligand_dirs = os.listdir('docked_ligand_pdbqt')
+
+    for i in ligand_dirs:
         for rank in range(1, 6):
             pool.apply_async(func, (i, rank))
 
@@ -22,4 +28,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
